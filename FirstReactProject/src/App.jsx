9acbf43512c24bf331ec89a247e46308950c4733
ghada <gh.abassi01@gmail.com>
2024-02-19 11:48:46 +0100

    AddTitle+Image

--- FirstReactProject/src/App.jsx
+++ FirstReactProject/src/App.jsx
@@ -3,31 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const title="React";
 function App() {
   const [count, setCount] = useState(0)
+  const welcome={
+    greeting:"Hey",
+    title:"React",
+  }
 
+  function getTitle(title){
+    return title;
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React {getTitle("Project")}</h1>
+      <img src='https://www.purina.co.uk/sites/default/files/2020-12/Cat%20Hunting%20BehavioursHERO.jpg' alt="catphoto" className='image'/>
+      <ul>
+        <li>Hy</li>
+        <li>Hi</li>
+      </ul>
+
+
     </>
   )
 }
